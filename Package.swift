e723c63a0ac491298c99ff8922f8158e42fcc777
Jenkins <jenkins@regula.by>
2026-06-29 18:17:19 +0300

    Release 9.6.18841

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["OCR"]),
     ],
     targets: [
-        .binaryTarget(name: "OCR", url: "https://pods.regulaforensics.com/OCR/9.5.18265/DocumentReaderCore_ocrandmrz_9.5.18265.zip", checksum: "98d4c1c710d0c8dbe9a7b62e78eb43cb1963db599fba3870897605240e655b38"),
+        .binaryTarget(name: "OCR", url: "https://pods.regulaforensics.com/OCR/9.6.18841/DocumentReaderCore_ocrandmrz_9.6.18841.zip", checksum: "0cf1ac372fccb732bd214c8966ef92e607f49b4d86c875a5538c7cbb45813440"),
     ]
 )
