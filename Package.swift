ae07339f98754c83d2a4f44d3d56e93db62fd2e1
Jenkins <jenkins@regula.by>
2026-08-01 03:53:12 +0300

    Stage 9.7.19625

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["OCRStage"]),
     ],
     targets: [
-        .binaryTarget(name: "OCRStage", url: "https://pods.regulaforensics.com/Stage/OCRStage/9.7.19596/DocumentReaderCoreStage_ocrandmrz_9.7.19596.zip", checksum: "0fdf3b92797ceafabe94ada187d3ddc8710a8c432eff678b602392cfc32ebd90"),
+        .binaryTarget(name: "OCRStage", url: "https://pods.regulaforensics.com/Stage/OCRStage/9.7.19625/DocumentReaderCoreStage_ocrandmrz_9.7.19625.zip", checksum: "ee9463164604ece947f13963d915dbbfc577c547be760c44a4eea1d87c66de00"),
     ]
 )
